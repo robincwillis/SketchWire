b5e69d93918246054eedda618ae58db5dc08ec43
robincwillis <robincwillis@gmail.com>
2013-07-02 13:17:14 -0700

    added preloader for video

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+
+ var video = document.getElementsByTagName('video')[0];
+
+video.addEventListener('loadeddata', function(e) { // Repeat this for other events
+	var preloader = document.getElementById("preloader");
+    preloader.parentNode.removeChild(preloader);
+
+});
+
+
+
